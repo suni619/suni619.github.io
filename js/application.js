460756237142807fdfc19a443f6046cc6f540f98
suni619 <suni619@gmail.com>
2014-08-25 21:17:22 -0500

    Website

---
+++ js/application.js
@@ -0,0 +1,11 @@
+/**
+ * jQuery functionalities of the site
+ */
+
+// set initial width and height
+$(document).ready(function() {
+	$("body").css({
+		"width" : $(window).width(),
+		"height" : $(window).height(),
+	});
+});
